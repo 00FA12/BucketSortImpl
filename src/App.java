242816c427789792f89dfc0a49f417c5359300ac
Redver <116087243+Redver@users.noreply.github.com>
2024-03-14 08:59:20 +0100

    made the list big for testing

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        BucketSort bs = new BucketSort(new int[]{4, 6, 7, 2, 5, 4});
-        bs.sort();
-
+        int[] bigList = new int[1000];
+        Random random = new Random();
+        for (int i = 0; i < 1000; i++) {
+            int rand = random.nextInt(1,10000);
+            bigList[i] = rand;
+        }
+        BucketSort bs = new BucketSort(bigList);
+        int[] sorted = bs.sort();
+        for (int var:sorted
+             ) {
+            Integer temp = var;
+            System.out.println(temp.toString());
+        }
         }
 }
